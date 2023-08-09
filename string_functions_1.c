056dc38e2a750750c281af3d9373f0f2dbda37ee
mr_drgh <mrdrgh2003@gmail.com>
2023-08-09 13:18:22 +0100

    added string functions 1

---
+++ string_functions_1.c
@@ -0,0 +1,100 @@
+#include "shell.h"
+
+/**
+ * _putchar - puts a char
+ * @c: the char to put
+ * Return: whatever
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _puts - using _putchar , print a string into stdout
+ * @s: the string to put out
+ * Return: the nbr of chars printed
+*/
+int _puts(char *s)
+{
+	char *p = s;
+	int len = 0;
+	
+	while (*p)
+	{
+		_putchar(*p);
+		p++;
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strlen - returns the len of the string
+ * @s: the string to checck
+ * Return: the  nbr of chars
+*/
+int _strlen(char *s)
+{
+	char *p = s;
+	int len = 0;
+	
+	while (*p)
+	{
+		p++;
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strcat - concates str2  into str1
+ * @str1: the result
+ * @str2: the str to concate
+ * Return: the result str 
+*/
+char *_strcat(char *str1, char *str2)
+{
+	char *p, *q;
+
+	p = str1;
+	q = str2;
+	while (*p)
+	{
+		p++;
+	}
+	while (*q)
+	{
+		*p = *q;
+		p++;
+		q++;
+	}
+	*p = '\0';
+	return (str1);
+}
+/**
+ * _strncat - concatenates the first n chars of src
+ * @dest: the destination
+ * @src: the source
+ * @n: the number of the chars
+ * Return: the destination
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *ppp = src;
+	char *qqq = dest;
+
+	while (*qqq)
+	{
+		qqq++;
+	}
+	while (i < n && *ppp)
+	{
+		*qqq = *ppp;
+		i++;
+		ppp++;
+		qqq++;
+	}
+	*qqq = '\0';
+	return (dest);
+}
+

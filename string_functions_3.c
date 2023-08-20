9d441a74a712e127caeb8308bf8703c67083e2b0
Mrdrgh <mrdrgh2003@gmail.com>
2023-08-20 12:17:52 +0100

    updated split_str_prime

--- string_functions_3.c
+++ string_functions_3.c
@@ -9,16 +9,18 @@
 char ***split_str_prime(char *line_buffer)
 {
 	char *f_token, *token_copy;
-	char ***token_of_tokens = malloc(sizeof(char**) * 1024);
-	int i = 0;
+	char ***token_of_tokens = NULL;
+	int count = 0;
 
 	f_token = _strtok_r(strdup(line_buffer), ";\n", &token_copy);
 	while (f_token)
 	{
-		token_of_tokens[i] = splitstr(f_token);
+		token_of_tokens = _realloc(token_of_tokens, count * sizeof(char **),
+		(count + 1) * sizeof(char **));
+		count++;
+		token_of_tokens[count - 1] = splitstr(f_token);
 		f_token = _strtok_r(NULL, ";\n", &token_copy);
-		i++;
 	}
-	token_of_tokens[i] = NULL;
+	token_of_tokens[count] = NULL;
 	return (token_of_tokens);
 }

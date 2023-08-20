b638964abf13f26b9f9b28caf945818c08373faf
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-08-20 04:48:47 +0100

    modified build-in getline function

--- _getline.c
+++ _getline.c
@@ -4,7 +4,7 @@
  * _getchar - reads a single character from stdin
  *
  * Return: The character read from stdin if successful, EOF otherwise
-*/
+ */
 int _getchar(void)
 {
 	int c;
@@ -22,31 +22,60 @@ int _getchar(void)
 	return c;
 }
 /*
- * This function gets a line of input from the user.
+ * _getline - gets a line of input from the user.
  *
  * @str - A pointer to the buffer that will store the input line.
  * @n - The maximum number of characters that can be stored in the buffer.
  * Returns:
  *   A pointer to the buffer that contains the input line.
  */
-
-char *_getline(char *str, int n)
+ssize_t getline(char **lineptr, size_t *n, FILE *stream)
 {
-	int i = 0;
-	int c = getchar();
+	char *buff = NULL;
+	size_t buff_size = 0, len_read = 0;
+	char c;
 
-	(void)n;
-	while (c != EOF && c != '\n')
+	if (lineptr == NULL || n == NULL || stream == NULL)
 	{
-		str[i] = c;
-		i++;
-		c = getchar();
+		return (-1);
 	}
-	if (c == '\n')
+	if (*lineptr == NULL)
 	{
-		str[i] = c;
-		i++;
+		*lineptr = malloc(*n * sizeof(char));
+		if (*lineptr == NULL)
+		{
+			return (-1);
+		}
 	}
-	str[i] = '\0';
-	return str;
+	buff = *lineptr;
+	buff_size = *n;
+
+	while ((c = _getchar() != EOF))
+	{
+		if (len_read + 1 >= buff_size)
+		{
+			buff_size += 128;
+			buff = _realloc(buff, *n, buff_size);
+			if (buff == NULL)
+			{
+				free(buff);
+				return (-1);
+			}
+		}
+		buff[len_read++] = c;
+		if (c == '\n')
+		{
+			break;
+		}
+	}
+	if (len_read == 0)
+	{
+
+		free(buff);
+		return (-1);
+	}
+	buff[len_read] = '\0';
+	*lineptr = buff;
+	*n = buff_size;
+	return (len_read);
 }
